b47839b18b3d082256d49507d39a06e231285997
quinn232024S <165211261+quinn232024S@users.noreply.github.com>
2024-10-27 18:22:39 -0400

    Add files via upload
    This it the coe that is currently on the FC_Board. The memory allocations on the FC_Board might be caused by the .mpy files which need to be recompiled.

---
+++ Backup Code/(IF)testing.py
@@ -0,0 +1,14 @@
+import board
+print(dir(board))
+
+print("")
+#print("A0:", board.A0)
+print("D0:", board.D0)
+print("")
+#print("VOLTAGE_MONITOR: ", board.VOLTAGE_MONITOR)
+print("RF1_RST:", board.RF1_RST)
+#print("A3: ", board.A3)
+print("")
+#print("GP11: ", board.GP11)
+print("")
+#print("GP15: ", board.GP15)
