e0c1eeaea1f234438922ff9f16d29a5fa0165fe1
ftlongsandwich <64384878+ftlongsandwich@users.noreply.github.com>
2025-02-16 16:49:30 -0500

    refectored joke to return an ax.25 data frame

--- Backup Code/lib/functions.py
+++ Backup Code/lib/functions.py
@@ -123,7 +123,11 @@ class functions:
         del Field
     
     def joke(self):
-        self.send(random.choice(self.jokes))
+        #self.send(random.choice(self.jokes))
+        joke = random.choice(self.jokes)
+        payload = bytearray([0x20])
+        payload.extend(joke.encode(encoding="utf-8")
+        return payload
     
 
     def state_of_health(self):
